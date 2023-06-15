a0f35c0f4e91badcdd5f446137c0f61232c766b0
crystalps <76597738+smhman@users.noreply.github.com>
2023-06-15 23:30:08 +0300

    Update Discord.tsx

--- src/components/Discord.tsx
+++ src/components/Discord.tsx
@@ -1,4 +1,4 @@
-import formatDistanceStrict from "date-fns/formatDistanceStrict"';
+import formatDistanceStrict from "date-fns/formatDistanceStrict";
 import Image from "next/future/image";
 import { useEffect, useState } from "preact/hooks";
 import { Activity, useLanyard } from "react-use-lanyard";
