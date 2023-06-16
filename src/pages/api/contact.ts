78496b7668f42ae4a5a592bf15c805a50776c234
crystalps <76597738+smhman@users.noreply.github.com>
2023-06-16 03:16:18 +0300

    Update contact.ts

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,53 +1,48 @@
+import {bwitch} from 'bwitch';
+import {NextkitError} from 'nextkit';
 import {z} from 'zod';
 import {api} from '../../server/api';
 import {DISCORD_WEBHOOK} from '../../server/constants';
+import {codeblock} from '../../utils/discord';
 
 const schema = z.object({
 	email: z.string().email(),
 	body: z.string().max(500).min(10),
+	turnstile: z.string(),
 });
 
 export default api({
-	async POST({req, res}) {
+	async POST({req}) {
 		const body = schema.parse(req.body);
+
+		const ip = (req.headers['x-forwarded-for'] as string) ?? req.socket.remoteAddress ?? null;
 
 		const result = await fetch(DISCORD_WEBHOOK, {
 			method: 'POST',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
-				content: 'new email innit',
+				content: 'contact form submission',
 				embeds: [
 					{
 						description: body.body,
-						author: {
-							name: body.email,
-						},
-						fields: [
-							{
-								name: 'ip',
-								value:
-									req.headers['x-forwarded-for'] ??
-									req.connection.remoteAddress ??
-									'unknown!?',
-							},
-						],
+						author: {name: body.email},
+						fields: [{name: 'ip', value: ip ?? 'unknown!?'}],
+					},
+
+					{
+						title: 'turnstile',
+						description: codeblock("Oy bruv, turnstile isnt working innit", 'json'),
 					},
 				],
 			}),
 		});
 
 		if (result.status >= 400) {
-			res.throw(result.status, 'Error sending notification');
+			throw new NextkitError(result.status, 'Error sending notification');
 		}
 
-		if (req.headers['content-type'] === 'application/json') {
-			return {
-				sent: true,
-			};
-		}
-
-		return {
-			_redirect: '/thanks',
-		};
+		return bwitch(req.headers['content-type'])
+			.case('application/json', () => ({sent: true}))
+			.or(() => ({_redirect: '/thanks'}));
 	},
 });
