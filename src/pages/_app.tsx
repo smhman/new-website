d7163aa19a0455186e386bff9275624963b07f38
crystalps <76597738+smhman@users.noreply.github.com>
2023-06-17 00:33:42 +0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,35 @@
 import "../styles/index.scss";
-
 import type { AppProps } from "next/app";
 import Head from "next/head";
-
 import Layout from "../components/Layout";
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Head>
-				<title>sundei</title>
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://sundei.eu/assets/js/weather.js';
+    document.body.appendChild(script);
 
-				<meta
-					name="viewport"
-					content="initial-scale=1.0, width=device-width"
-				/>
-				<meta name="theme-color" content="#000000" />
+    return () => {
+      // Cleanup if necessary
+      document.body.removeChild(script);
+    };
+  }, []);
 
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<script src="https://sundei.eu/assets/js/weather.js"></script>
-			</Head>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>sundei</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
 export default MyApp;
