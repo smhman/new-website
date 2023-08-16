e56e874396950a29992ef26d1f78487fcf625c17
sundei <ajaleht@pg.edu.ee>
2023-08-16 08:35:57 +0300

    Update GenericMeta.tsx

--- src/components/GenericMeta.tsx
+++ src/components/GenericMeta.tsx
@@ -14,7 +14,6 @@ export default function GenericMeta({ title, description }: GenericMetaProps) {
 			<meta property="og:description" content={description} />
 			<meta property="og:image" content="https://media.discordapp.net/attachments/1097585822414409792/1107305649383149759/Bojan_lick_DCB.gif" />
 			<meta content="#f0a1b3" data-react-helmet="true" name="theme-color" />
-			</meta>
 		</Head>
 	);
 }
