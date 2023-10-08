d9c30b546040d27cedaf4e2d617ded61c1f038d3
sundei <sundei@sundei.ee>
2023-10-09 01:08:31 +0300

    Update Clock.tsx

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,33 +1,36 @@
 import { useEffect, useState } from "preact/hooks";
 
 const formatter = new Intl.DateTimeFormat("en-US", {
-	day: "numeric",
-	weekday: "long",
-	year: "numeric",
-	month: "long",
-	hour: "2-digit",  // Use 2-digit format for the hour
-	minute: "2-digit",
-	second: "2-digit",
-	hour12: false,
-	timeZone: "Europe/Copenhagen",
-	timeZoneName: "short"
+  day: "numeric",
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hour12: false,
+  timeZone: "Europe/Copenhagen",
+  timeZoneName: "short"
 });
 
 export function Clock() {
-	const [now, setNow] = useState(0);
+  const [now, setNow] = useState(0);
 
-	useEffect(() => {
-		setNow(Date.now());
+  useEffect(() => {
+    setNow(Date.now());
 
-		const id = setInterval(() => {
-			setNow(Date.now());
-		}, 1000);
+    const id = setInterval(() => {
+      setNow(Date.now());
+    }, 1000);
 
-		return () => clearInterval(id);
-	}, []);
+    return () => clearInterval(id);
+  }, []);
 
-	const formattedTime = formatter.format(now);
-	const displayTime = formattedTime.replace(" at", " ·");
+  const formattedTime = formatter.format(now);
+  const displayTime = formattedTime.replace(" at", " ·");
 
-	return <>{displayTime.replace("00:", "24:")}</>;
+  // Replace "24:00" with "00:00"
+  const adjustedTime = displayTime.replace("24:00", "00:00");
+
+  return <>{adjustedTime}</>;
 }
