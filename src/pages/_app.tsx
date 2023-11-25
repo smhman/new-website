8b8a28e2d30bf49654be0aba6b7d0f01a0f6493a
sundei <sundei@sundei.ee>
2023-11-26 00:13:53 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import Layout from "../components/Layout";
 import { useEffect } from 'react';
-import $ from 'jquery';
+import $ from '@types/jquery';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
