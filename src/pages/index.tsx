a677e16e1aa7abf40075f3d7e30a29027d4399ee
sundei <sundei@sundei.ee>
2023-11-25 22:00:40 +0200

    Added Ты Расскажи Мне embed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -75,6 +75,7 @@ export default function Home({
 
 			<Discord />
 			<Spotify />
+			<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7aiGJOMa4E2qr3l7wRCnLt?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 			<br/>
 			<br/>
 		</>
