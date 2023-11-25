8725f1d5cd931c93e9124fac64bffcf3594fbc8c
sundei <sundei@sundei.ee>
2023-11-25 23:25:02 +0200

    Remaking stuff.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,18 +32,10 @@ export default function Home({
 			<h1 className="heading mb-2">
 				sundei! <HomeIcon className="ml-4 h-12 w-12 text-orange-400" />
 			</h1>
+			<p className="mb-4">
+				Hi there! I'm sundei, a <span className="text-cyan-400">{age}-year-old developer and IT specialist</span> from <span className="text-teal-400">Tartu, Estonia</span>. Currently enrolled at <span className="text-lime-400">Tartu Vocational College</span> and unofficially serving as tech support at <span className="text-emerald-400">Universal Music Estonia</span>.
+			</p>
 
-			<p className="mb-4">
-				Hi! I&apos;m sundei, a{" "}
-				<span className="text-blue-400">{age}-year-old developer & IT-specialist</span>{" "}
-				from <span className="text-rose-400">Tartu</span>, Estonia.
-			</p>
-			<p className="mb-4">
-			I am currently studying at{" "}<span className="text-blue-400">Tartu Vocational College</span>.
-			</p>
-			<p className="mb-4">
-			I am unofficial tech support at{" "}<span className="text-blue-400">Universal Music Estonia</span>.
-			</p>
 
 			<p className="mb-2 flex flex-wrap gap-2 items-center">
 				{socials.map(({ name, image, url }) => (
@@ -79,7 +71,8 @@ export default function Home({
 			<p></p>
 			<hr className="mb-4 bg-slate-800 border-none h-0.5" />
 			<p className="mb-4">
-			Currently favourite album:{" "}<span className="text-blue-400">&quot;ALIKA&quot;</span> by{" "}<span className="text-blue-400">Alika Milova</span>. Listen to it down below!
+			Currently my favorite album is <span className="text-violet-800">&ldquo;ALIKA&rdquo;</span> by <span className="text-blue-800">Alika Milova</span>. 
+			Currently my favorite song is <span className="text-rose-400">&ldquo;Ты Расскажи Мне&rdquo;</span> by <span className="text-pink-500">Alika Milova</span> from album <span className="text-rose-600">&ldquo;ALIKA&rdquo;</span>. Listen to it and other songs from album down below!
 			</p>
 			<iframe style={{ borderRadius: '12px' }} src="https://open.spotify.com/embed/album/2qGwXvw1kIv5zPpwqXVLjR?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 			</div>
