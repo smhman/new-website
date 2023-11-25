de69ac006e3e56e18de0d780117c9baef8a1dbdf
sundei <sundei@sundei.ee>
2023-11-25 22:59:13 +0200

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -10,13 +10,13 @@ export default function Custom404() {
 				Seems like this page doesn&apos;t exist. Let&apos;s{" "}
 				<Link href="/">
 					<a className="border-b border-[#fff4] hover:border-white transition">
-						go back... Or listen to &quot;ALIKA&quot;.
+						go back... Or listen to &quot;ALIKA&quot;
 					</a>
 				</Link>
 				<div>
 					<iframe style={{ borderRadius: '12px' }} src="https://open.spotify.com/embed/album/2qGwXvw1kIv5zPpwqXVLjR?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 				</div>
-				!
+				
 			</p>
 		</>
 	);
